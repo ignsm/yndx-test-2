dedac98379a568ca51aaa1a482a5fdb8920c2e8f
ignsm <ignsm@yandex.ru>
2017-09-26 22:26:10 +0300

    feat: removeClass()

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
   let cssDom = function(selector) {
     return new cssDom.prototype.nodes(selector);
   };
+  /** Находим необходимый селектор */
   cssDom.prototype.nodes = function(selector) {
     let nodes;
 
@@ -30,12 +31,26 @@
 
   cssDom.prototype.nodes.prototype = cssDom.prototype;
 
+  /** Даём класс нашим нодам */
   cssDom.prototype.addClass = function(str) {
     let elem = this;
     for (let i = 0; i < elem.length; i++) {
       elem[i].classList
         ? elem[i].classList.add(str)
         : (elem[i].className += " " + str);
+    }
+  };
+
+  /** Удаляем класс нашим нодам */
+  cssDom.prototype.removeClass = function(str) {
+    let elem = this;
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].classList
+        ? elem[i].classList.remove(str)
+        : (elem[i].className = elem[i].className.replace(
+            new RegExp("(^|\\b)" + str.split(" ").join("|") + "(\\b|$)", "gi"),
+            " "
+          ));
     }
   };
 
